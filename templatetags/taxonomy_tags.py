0f4cfc6379049253035f1c8d38c271a30a6e4edc
Justin Michalicek <justin@bash-shell.net>
2011-09-13 14:56:01 -0400

    * Added README about taxonomy app * taxonomy_members template tag can take variables or literal values for args

--- templatetags/taxonomy_tags.py
+++ templatetags/taxonomy_tags.py
@@ -6,7 +6,7 @@ register = template.Library()
 
 def get_taxonomy_members(parser, token):
     try:
-        bits = token.contents.split()
+        bits = tuple(token.split_contents())
     except ValueError:
         raise template.TemplateSyntaxError(
             "%r tag requires arguments" % token.contents.split()[0]
@@ -22,17 +22,33 @@ def get_taxonomy_members(parser, token):
 
 class TaxonomyMembers(template.Node):
     def __init__(self, taxonomy_group, taxonomy_item, var_name="members"):
-        self.taxonomy_item_name = template.Variable(taxonomy_item)
-        self.taxonomy_group_name = template.Variable(taxonomy_group)
+        # Most examples set variables using template.Variable here
+        # and then resolve() in render().  I am doing both in render due to the
+        # method call to easily deal with the potential for an arg being
+        # a variable or a literal value.
+        self.taxonomy_group = taxonomy_group
+        self.taxonomy_item = taxonomy_item
         self.variable_name = var_name
 
     def render(self, context):
-        resolved_name = self.taxonomy_item_name.resolve(context)
-        resolved_group = self.taxonomy_group_name.resolve(context)
+        resolved_name = resolve_variable(self.taxonomy_item, context)
+        resolved_group = resolve_variable(self.taxonomy_group, context)
+
         taxonomy_item = TaxonomyItem.objects.get(name=resolved_name,
                                                  taxonomy_group__name=resolved_group)
         context[self.variable_name] = taxonomy_item.get_members()
         return ''
 
+def resolve_variable(arg, context):
+    """Attempt to resolve a template param to a template variable value
+    or return the param as is if it can't be resolved because it's a real value
+    and not a variable"""
+
+    try:
+        value = template.Variable(arg).resolve(context)
+    except template.VariableDoesNotExist:
+        value = arg
+
+    return value
 
 register.tag('taxonomy_members', get_taxonomy_members)
