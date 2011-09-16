cab88451515d41506743259ec6a21bbb1c5cb9ca
Justin Michalicek <justin@bash-shell.net>
2011-09-15 21:21:04 -0400

    * All unit tests for taxonomy app are in place including template tag testing

--- tests.py
+++ tests.py
@@ -12,12 +12,15 @@ from django.conf import settings
 from django.core.management import call_command
 from django.db.models import loading
 from django import test
+from django.template import Template, Context, Parser
+
+from mock import patch, Mock
 
 from taxonomy.models import *
 
-#TODO:
-# test for get_taxonomy_members and the taxonomy_members template tag
-# test for TemplateItem.add_member and get_members - look at http://stackoverflow.com/questions/5097937/emulating-an-app-with-models-in-a-django-unittest
+# Idea to use mock for testing template tags from the links below
+# http://techblog.ironfroggy.com/2008/10/how-to-test-django-template-tags-part-1.html
+# http://techblog.ironfroggy.com/2008/10/how-to-test-django-template-tags-part-2.html
 
 class TaxonomyTests(TestCase):
     def setUp(self):
@@ -39,25 +42,55 @@ class TaxonomyTests(TestCase):
         #by the other tests
         taxonomy_item.add_member(test_model)
 
+        self.group_name = 'Test Group'
+        self.model_name = 'Test Model'
+        self.item_name = 'Test Taxonomy Item'
+
         self.taxonomy_group = taxonomy_group
         self.taxonomy_item = taxonomy_item
         self.test_model = test_model
 
 
     def test_get_taxonomies(self):
-        taxonomies = self.test_model.get_taxonomies(self.taxonomy_group)
+        tt = TaxonomyTest.objects.get(name=self.model_name)
+        taxonomies = tt.get_taxonomies(self.taxonomy_group)
         self.assertTrue(hasattr(taxonomies,'__iter__'))
         self.assertEqual(taxonomies[0].pk, self.taxonomy_item.pk)
 
     def test_get_taxonomy_groups(self):
-        tgroups = self.test_model.get_taxonomy_groups()
+        tt = TaxonomyTest.objects.get(name=self.model_name)
+        tgroups = tt.get_taxonomy_groups()
         self.assertTrue(hasattr(tgroups,'__iter__'))
         self.assertEqual(tgroups[0].pk, self.taxonomy_group.pk)
 
     def test_get_members(self):
-        members = self.taxonomy_item.get_members()
+        ti = TaxonomyItem.objects.get(name=self.item_name)
+        members = ti.get_members()
         self.assertTrue(hasattr(members,'__iter__'))
         self.assertEqual(members[0].pk, self.test_model.pk)
 
-        #figure out how to test the template tag - need to parse a template
+    def test_get_members_tag(self):
+        """This tests the get_taxonomy_members method which is directly called by 
+        the get_members tag and so tests instantion of the TaxonomyMembers object.
+        It also tests to make sure the TaxonomyMembers' render() method works"""
+
+        parser = Parser(None)
+        token = Mock(spec=['split_contents'])
+        
+        token.split_contents.return_value = ('get_members', self.taxonomy_group.name,
+                                             self.taxonomy_item.name)
+
+
+        from templatetags.taxonomy_tags import get_taxonomy_members
+        node = get_taxonomy_members(parser,token)
+        self.assertEqual(node.taxonomy_group, self.taxonomy_group.name)
+        self.assertEqual(node.taxonomy_item, self.taxonomy_item.name)
+
+        # It feels like this part below should be its own test
+        # but the above needs to be done to test it, so might as well
+        # just consider it all one test
+        c = Context()
+        node.render(c)
+        self.assertTrue(hasattr(c['members'],'__iter__'))
+        self.assertEqual(c['members'][0].pk, self.test_model.pk)
 
